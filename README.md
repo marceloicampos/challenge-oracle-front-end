e4eaad7e12d59363bf4c1269bcbf25390dc94864
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-03-27 16:58:49 -0300

    project update

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Challenge Oracle Front End
 
 20/02/2022 - Inicio do Projeto
 
+27/03/2022 - Entrega do Projeto
+
 ## [Preview - CLIQE AQUI](https://marceloicampos.github.io/challenge-oracle-front-end/)
 
 ![](https://raw.githubusercontent.com/marceloicampos/challenge-oracle-front-end/main/preview.png)
--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 // var nome = 'marcelo'
 // console.log(nome)
 
-function validarDados() {
-    window.event.preventDefault()
-
+function inputValidation() {
     if (document.form.nome.value == '') {
         alert('Campo Nome Obrigatório')
         document.form.nome.focus()
     } else if (document.form.email.value == '') {
         alert('Campo E-mail Obrigatório')
         document.form.email.focus()
-    } else if (document.form.email.value.indexOf('@') == -1 || document.form.email.value.indexOf('.') == -1) {
+    } else if (document.form.email.value.indexOf('@') == -1) {
         alert('Digite um E-mail Válido')
         document.form.email.focus()
     } else if (document.form.assunto.value == '') {
@@ -20,5 +18,20 @@ function validarDados() {
         alert('A Mensagem é Obrigatória')
         document.form.mensagem.focus()
     }
+    if (document.form.nome.value == '' || document.form.email.value == '' || document.form.assunto.value == '' || document.form.mensagem.value == '') {
+        window.event.preventDefault()
+    }
+    if (document.form.nome.value > 0 && document.form.email.value > 0 && document.form.assunto.value > 0 && document.form.mensagem.value > 0) {
+        window.event.stopPropagation()
+    }
 }
-document.querySelector('form').addEventListener('submit', validarDados)
+
+document.querySelector('form').addEventListener('submit', inputValidation)
+
+function copyMail() {
+    const clip = navigator.clipboard
+    const text = document.querySelector('#look')
+    clip.writeText(text.innerText).then(() => alert('E-mail Copiado'))
+}
+
+document.querySelector('#look').onclick = copyMail
